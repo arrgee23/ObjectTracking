6ed0cfd51dd1ca0fa31aa13e7c7a2e027777cc35
Aniruddha-Mukherjee <aniruddhamukherjee017@gmail.com>
2016-03-19 20:29:53 +0530

    syria video tracking adaptive Threshold

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,11 +1,14 @@
 #include "opencv2/opencv.hpp"
+#include<iostream>
+#include<cstdio>
+
 
 using namespace cv;
 using namespace std;
 
 int main(int, char**)
 {
-    VideoCapture cap("input/input%02d.jpg"); // open the default camera
+    VideoCapture cap("Video/Simulated.mp4"); // open the default camera
 
     if(!cap.isOpened())  // check if we succeeded
         return -1;
@@ -22,10 +25,8 @@ int main(int, char**)
 
 
 	//cout<<dummyFrame.channels(); 3 channels
-
-    // convert to greyscale
-    cvtColor(dummyFrame,dummyFrame,CV_BGR2GRAY);
-    medianBlur(dummyFrame,dummyFrame,7); // blur it to remove noise
+    cvtColor(dummyFrame,dummyFrame,CV_BGR2GRAY);    // convert to greyscale
+      medianBlur(dummyFrame,dummyFrame,7);          // blur it to remove noise
 
     // cout<<dummyFrame.channels(); 1 channel
 
@@ -34,14 +35,9 @@ int main(int, char**)
     for(int i=0;i<256;i++)
     	histogram[i]=0;
 
-    for(int j=0;j<dummyFrame.rows;j++) 
-	{
+    for(int j=0;j<dummyFrame.rows;j++)
   		for (int i=0;i<dummyFrame.cols;i++)
-  		{
-    		   
-       		histogram[dummyFrame.at<uchar>(j,i)]++;
-  		}
-	}
+       		 histogram[dummyFrame.at<uchar>(j,i)]++;
 
 	// find most used colour for thresholding
 	int maxcount = 0;
@@ -51,6 +47,8 @@ int main(int, char**)
 
 	/* maxcount is the color of the sky */
 
+
+//cout<<"maxcount = "<<maxcount<<endl;
 
     for(;;)
     {
@@ -66,14 +64,15 @@ int main(int, char**)
         	break;
 
 
+        cvtColor(frame, dest, CV_BGR2GRAY);  // converts image from rgb(src) to gray level (dst)
 
-        cvtColor(frame, dest, CV_BGR2GRAY);  // converts image from rgb(src) to gray level (dst) 
-       	threshold(dest, bin, 40, 255, CV_THRESH_BINARY_INV); // // Tresholds image with level = 40 from gray level(dst) to binary (bin)
-       	imshow("threshold",bin);
+        adaptiveThreshold(dest, bin,255,CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY_INV,75,20);
+       	//threshold(dest, bin, 40, 255, CV_THRESH_BINARY_INV); // // Tresholds image with level = 40 from gray level(dst) to binary (bin)
+
+        imshow("threshold",bin);
        	findContours(bin,contours, hierarchy,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE ); // finds contours on bin image
-
        	Scalar color( 255,255,255 );
-       	for( int i = 0; i< contours.size(); i++ ) // iterate through each contour. 
+       	for( int i = 0; i< contours.size(); i++ ) // iterate through each contour.
        	  {
        	   if((contourArea(contours[i],false))>100){ // if counter area >100 pixel draw it on ero which is new image variable
        	    drawContours( bin, contours, i , color, CV_FILLED, 8, hierarchy ); //Draw contours on itself as filled
@@ -82,17 +81,19 @@ int main(int, char**)
 
        	findContours( bin, contours, hierarchy,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE );
 
-       	 for( int i = 0; i< contours.size(); i++ ) // iterate through each contour. 
+       	 for( int i = 0; i< contours.size(); i++ ) // iterate through each contour.
        	  {
        	      bounding_rect=boundingRect(contours[i]); //Bound and Draw rectangle each object which detected at the end on src(original image)
-       	      rectangle(frame, bounding_rect,  Scalar(0,255,0),3, 8,0);  
+       	      rectangle(frame, bounding_rect,  Scalar(0,255,0),3, 8,0);
        	   }
-       	   
-       	   imshow("original",frame);
-       	   cout<<contours.size();
 
-        if(waitKey(500) >= 0) break;
+       	   imshow("original",frame);
+       	   //cout<<contours.size()<<"\t";
+
+           waitKey(20);
+        //if(waitKey(500) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
+
     return 0;
 }
